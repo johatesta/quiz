518679ab39852750881d00b134aca5299603ba36
johatesta <johatesta@gmail.com>
2022-01-12 22:17:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+import Trivia from './components/Trivia';
+function App() {
+
+  const [questionNumner, setQuestionNumber] = useState(1)
+
+  const moneypyramid =[
+    {id:1, amount:"$100"},
+    {id:2, amount:"$200"},
+    {id:3, amount:"$300"},
+    {id:4, amount:"$500"},
+    {id:5, amount:"$1000"},
+    {id:6, amount:"$2000"},
+    {id:7, amount:"$4000"},
+    {id:8, amount:"$8000"},
+    {id:9, amount:"$16000"},
+    {id:10, amount:"$32000"},
+    {id:11, amount:"$64000"},
+    {id:12, amount:"$125000"},
+    {id:13, amount:"$250000"},
+    {id:14, amount:"$25000"},
+    {id:15, amount:"$1000000"}].reverse();
+
+
+  return (
+    <div className="App">
+     <div className='main'>
+       <div className="top"></div>
+       <div className="bottom"><Trivia/></div>
+     </div>
+     <div className='pyramid'>
+       <ul className='moneylist'>
+         {moneypyramid.map((m)=>(<li className={questionNumner===m.id ?"moneylistitem active" : "moneylistitem"} >
+          
+          <span className='moneylistamount'>{m.amount}</span>
+        </li>))}
+       
+         
+
+       </ul> 
+     </div>
+    </div>
+  );
+}
+
+export default App;
